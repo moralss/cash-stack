f5e2bf3db86eb7f2eda87b20bb92cd7de730f06a
moralss <jeramoral@gmail.com>
2019-10-22 15:54:50 +0200

    add

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  //   app.use(express.static(path.join(__dirname, 'client/build')));
   app.use(express.static('client/build'));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
   })
 }
 
